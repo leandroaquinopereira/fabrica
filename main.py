ae9523fc8f1a64ca5fb43bbb4c2c1c37dfd56ba5
Leandro de Aquino Pereira <leandro.pereira@inatel.br>
2022-11-29 13:54:39 -0300

    Continuacao

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ df_branch = pd.read_excel('files/branch.xlsx', engine='openpyxl')
 compare_values = df_prod.values == df_branch.values
 
 print(compare_values)
+
+rows, cols = np.where(compare_values == False)
+
+for item in zip(rows, cols):
+    df_prod.iloc[item[0], item[1]] = ' {} --> {} '.format(df_prod.iloc[item[0], item[1], df_branch.iloc[item[0], item[1]]])
+
+df_prod.to_excel('files/output.xls', index=False, header=True)
