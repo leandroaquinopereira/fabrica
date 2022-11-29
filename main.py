ac3710b8f11bced267d2bcd431027774e00dbe60
Leandro de Aquino Pereira <leandro.pereira@inatel.br>
2022-11-29 13:30:29 -0300

    Main changes

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import numpy as np
+
+df_prod = pd.read_excel('files/master.xlsx', engine='openpyxl')
+df_branch = pd.read_excel('files/branch.xlsx', engine='openpyxl')
+
+compare_values = df_prod.values == df_branch.values
+
+print(compare_values)
